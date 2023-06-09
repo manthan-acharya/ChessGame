fbb328e74962755f50876ec1e1b23b7a7f72c4e5
JosephK2024 <135837197+JosephK2024@users.noreply.github.com>
2023-06-09 09:21:53 -0400

    Update ChessBoard.java
    method added to print the board into the console,
    method added to find piece location (not finished)

--- ChessBoard.java
+++ ChessBoard.java
@@ -8,5 +8,41 @@ public class ChessBoard {
       board = new ChessPiece[8][8];
 
    }
+   
+   public void printBoard() {
+        System.out.println("---------------------------------");
+        for (int row = 0; row<8; row++) {
+            for (int col = 0; col<=7; col++) {
+                System.out.print("| ");
+                if (isPieceAtLocation(row, col))
+                {
+                    
+                }
+                else if ((row + col) % 2 == 0)
+                {
+                    System.out.print('\u25A0');
+                }
+                else
+                {
+                    System.out.print('\u25A1');
+                }
+                System.out.print(" ");
+            }
+            System.out.println("|");
+            System.out.println("---------------------------------");
+        }
+    }
 
+    public boolean isPieceAtLocation(int row, int col)
+    {
+        for(ChessPiece[] boardRow : board){
+            for(ChessPiece peice : boardRow)
+                {
+                    //if (piece.getLocation() == {row, col}
+                    //    return true;
+                    //
+                }
+        }
+        return false;
+    }
 }
