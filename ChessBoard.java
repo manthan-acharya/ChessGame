70b976908d0bd485efba6f00c7a1513f37f98ce5
lukem232 <14318745+lukemagdalin@users.noreply.github.com>
2023-06-11 01:18:21 -0400

    Minor fixes, Added toString method

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,44 +1,54 @@
 //chess board class which holds the chess pieces in a chess board object
 
-public class ChessBoard {
+import java.awt.*;
 
+public class ChessBoard
+{
    private ChessPiece[][] board;
 
+   /**
+    * Creates a new chess board with the given board
+    * @param board (ChessPiece[][]) - the board to clone this board to
+    */
+   public ChessBoard(ChessPiece[][] board)
+   {
+      this.board = board.clone();
+   } // end setBoard(ChessPiece[][]);
 
+    /**
+     * Creates a new chess board with an empty board
+     */
+    public ChessBoard()
+   {
+      board = new ChessPiece[8][8];
+   } // end ChessBoard();
+
+    /**
+     * Gets this chess board
+     * @return (ChessPiece[][]) - this board
+     */
    public ChessPiece[][] getBoard()
    {
       return board;
-   }
+   } // end getBoard();
 
-   /**
-    * Sets the chess board to a given board
-    * @param board
-    */
-   public void setBoard(ChessPiece[][] board)
-   {
-      this.board = board.clone();
-   }
-
-   public ChessBoard() {
-      board = new ChessPiece[8][8];
-   }
-   
    public void printBoard() {
         System.out.println("---------------------------------");
         for (int row = 0; row<8; row++) {
             for (int col = 0; col<=7; col++) {
                 System.out.print("| ");
-                if (isPieceAtLocation(row, col))
+                Point location = new Point(row, col);
+                if (isLocationOccupied(location))
                 {
                     
                 }
                 else if ((row + col) % 2 == 0)
                 {
-                    System.out.print('\u25A0');
+                    System.out.print('■');
                 }
                 else
                 {
-                    System.out.print('\u25A1');
+                    System.out.print('□');
                 }
                 System.out.print(" ");
             }
@@ -47,15 +57,100 @@ public class ChessBoard {
         }
     }
 
-    public boolean isPieceAtLocation(int row, int col)
+    /**
+     * Determines if a piece is present at a given location
+     * @param location (Point) - the location to check
+     * @return (boolean) - if a piece is at the location
+     */
+    public boolean isLocationOccupied(Point location)
     {
-        for(ChessPiece[] boardRow : board){
-            for(ChessPiece peice : boardRow)
+        ChessPiece pieceAtLocation = board[location.x][location.y];
+        boolean isPieceEmpty = pieceAtLocation.equals(ChessPiece.ChessPieceType.EMPTY);
+        return !isPieceEmpty;
+    } // end isLocationOccupied(Point);
+
+    /**
+     * Converts this board to a string
+     * @return (String) - the string representation of this board
+     */
+    public String toString()
+    {
+        // Create stringBuilder to append to, and add top row divider
+        StringBuilder boardString = new StringBuilder();
+        boardString.append("  " + rowDivider() +"\n");
+
+        // Iterate through each row of the board
+        int rowNumber = board.length;
+        for(ChessPiece[] row : board)
+        {
+            // Add row number and decrement it by one
+            boardString.append(rowNumber--).append(" | ");
+
+            // Iterate through each column of the board
+            for(ChessPiece piece : row)
+            {
+                // Get piece character
+                String chessPiece = piece.getType().getUnicode();
+
+                // If the piece is empty, set the character to a space
+                if(piece.getType().equals(ChessPiece.ChessPieceType.EMPTY))
                 {
-                    if (piece.getLocation() == {row, col}
-                        return true;
+                    chessPiece = " ";
+                }
+
+                // Append the character of the piece to the string with a column divider
+                boardString.append(chessPiece).append(" | ");
+            }
+            // add row divider
+            boardString.append("\n  " + rowDivider() +"\n");
+        }
+        // add column numbering
+        boardString.append("    a   b   c   d   e   f   g   h\n");
+
+        return boardString.toString();
+    } // end toString();
+
+    /**
+     * Returns the row divider for this board.
+     * Uses the length of the board to determine the number of dashes to add,
+     * account for the column dividers and the spaces between the pieces.
+     * @return (String) - the row divider
+     */
+    private String rowDivider()
+    {
+        // Calculate the number of dashes needed
+        int numberOfDashes = (board.length * 4) + 1;
+
+        // Create a stringBuilder to append to, and add the dashes
+        StringBuilder rowDivider = new StringBuilder();
+        for(int i = 0; i < numberOfDashes; i++)
+        {
+            rowDivider.append("-");
+        }
+        return rowDivider.toString();
+    } // end rowDivider();
+
+    /**
+     * Returns the piece at a given location
+     * Uses ChessPiece memory location to compare pieces
+     * @param piece (ChessPiece) - the piece to find
+     * @return (Point) - the location of the piece
+     */
+    private Point getPieceLocation(ChessPiece piece)
+    {
+        // Iterate through each row of the board
+        for(int boardRow = 0; boardRow < board.length; boardRow++)
+        {
+            // Iterate through each column of the board
+            for(int boardCol = 0; boardCol < board[boardRow].length; boardCol++)
+                {
+                    // Compare the memory location of the piece to the memory location of the piece at the current location
+                    if (piece ==  board[boardRow][boardCol])
+                    {
+                        return new Point(boardRow, boardCol);
+                    }
                 }
         }
-        return false;
-    }
+        return null;
+    } // end getPieceLocation(ChessPiece);
 }
