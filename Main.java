bf9ce4e2ea74b900cd223a08fdc67cad95f3ce71
lukem232 <14318745+lukemagdalin@users.noreply.github.com>
2023-06-15 14:19:16 -0400

    Add test methods and play game

--- Main.java
+++ Main.java
@@ -1,10 +1,38 @@
+import java.awt.desktop.SystemSleepEvent;
+import java.util.Objects;
+
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+//        testPrintPresets();
+//        testPrintNotationKey();
+        playGame();
+    }
 
+    public static void playGame()
+    {
+        ChessGame game = new ChessGame();
+        game.playGame();
+    }
 
+    public static void testPrintPresets()
+    {
+        // load presets
+        PresetManager.loadPresets();
 
-        // Create chess game
+        // print each preset name and the board
+        for (String presetName : PresetManager.getPresetNames())
+        {
+            System.out.println(presetName);
+            ChessBoard board = new ChessBoard(Objects.requireNonNull(PresetManager.getPreset(presetName)));
+            System.out.println(board);
+            System.out.println();
+        }
+    }
+
+    public static void testPrintNotationKey()
+    {
         ChessGame.printNotationKey();
     }
 }
