e9b764a7fbbf40ba383af0ee583681cd20205ebf
lukem232 <14318745+lukemagdalin@users.noreply.github.com>
2023-06-15 07:29:38 -0400

    Test notation key

--- Main.java
+++ Main.java
@@ -1,23 +1,10 @@
-import java.util.Objects;
-
 public class Main
 {
     public static void main(String[] args) {
 
-        // Load the presets
-        PresetManager.loadPresets();
 
-        // Print out the names of the presets
-        for(String name : PresetManager.getPresetNames())
-        {
-            System.out.println(name);
-        }
-        System.out.println();
 
-        // Create a new chess board with the default preset
-        ChessBoard board = new ChessBoard(PresetManager.getPreset("default"));
-
-        // Print out the board
-        System.out.println(board);
+        // Create chess game
+        ChessGame.printNotationKey();
     }
 }
